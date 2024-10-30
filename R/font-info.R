d3270ac3e5715d363f5052d5f31f0523804225b2
mike <mikefc@coolbutuseless.com>
2024-10-30 20:47:57 +1000

    function to get a lofi font

--- R/font-info.R
+++ R/font-info.R
@@ -1,31 +1,5 @@
 
 
-
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' #' List of included bitmap fonts. 
-#' #' 
-#' #' Each font is an object with class 'lofifont'
-#' #' 
-#' #' \describe{
-#' #'   \item{coords}{data.frame}
-#' #' }
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' "bitmap_fonts"
-#' 
-#' 
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' #' List of included vector fonts. 
-#' #' 
-#' #' Each font is an object with class 'lofifont'
-#' #' 
-#' #' \describe{
-#' #'   \item{coords}{data.frame}
-#' #' }
-#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' "vector_fonts"
-
-
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' Return the names of all included fonts
 #' @param type font type. Either 'bitmap' or 'vector'.
@@ -37,7 +11,25 @@
 get_lofi_names <- function(type) {
   if (type == 'bitmap') {
     names(bitmap_fonts)
-  } else {
+  } else if (type == 'vector') {
     names(vector_fonts)
+  } else {
+    c(names(bitmap_fonts), names(vector_fonts))
   }
 }
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Fetch an included 'lofi' font
+#' @param font_name Name of font e.g. 'unifont'
+#' @return 'lofi' font object
+#' @examples
+#' get_lofi_font('unifont')
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+get_lofi_font <- function(font_name) {
+  bitmap_fonts[[font_name]] %||% 
+    vector_fonts[[font_name]] %||% 
+    stop("No such font: ", font_name)
+}
+
